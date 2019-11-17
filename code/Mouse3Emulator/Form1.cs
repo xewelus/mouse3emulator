9525b94bc69f0be98a0cc5a00f735b3de6b6783a
xewelus <xewelus@gmail.com>
2019-11-17 15:47:35 +0300

    -

---
+++ code/Mouse3Emulator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using MouseKeyboardLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mouse3Emulator
+{
+	public partial class Form1 : Form
+	{
+		MouseHook mouseHook = new MouseHook();
+		KeyboardHook keyboardHook = new KeyboardHook();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			keyboardHook.KeyDown += new KeyEventHandler(keyboardHook_KeyDown);
+            keyboardHook.Start();
+		}
+
+		private void keyboardHook_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.BrowserForward)
+			{
+				MouseSimulator.Click(MouseButton.Middle);
+			}
+		}
+	}
+}
